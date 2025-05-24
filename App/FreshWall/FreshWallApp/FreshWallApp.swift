0afcb20546cfb39178035093d5ad3d1d9f5e329c
hi2gage <hi2gage.pdx@gmail.com>
2025-05-24 10:00:20 -0600

    Add xcode Project

---
+++ App/FreshWall/FreshWallApp/FreshWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshWallApp.swift
+//  FreshWall
+//
+//  Created by Gage Halverson on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FreshWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/FreshWall/FreshWallTests/FreshWallTests.swift
@@ -0,0 +1,16 @@
+//
+//  FreshWallTests.swift
+//  FreshWallTests
+//
+//  Created by Gage Halverson on 5/24/25.
+//
+
+import Testing
+
+struct FreshWallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
