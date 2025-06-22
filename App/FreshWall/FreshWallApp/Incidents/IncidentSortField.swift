cebfc398d770c3f66e371393caa092cdf1b7d41c
hi2gage <hi2gage.pdx@gmail.com>
2025-06-21 22:20:38 -0600

    Do incident

--- App/FreshWall/FreshWallApp/Incidents/IncidentSortField.swift
+++ App/FreshWall/FreshWallApp/Incidents/IncidentSortField.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 /// Fields available for sorting incidents.
-enum IncidentSortField {
+enum IncidentSortField: SortFieldRepresentable {
     case alphabetical
     case date
+
+    var label: String {
+        switch self {
+        case .alphabetical: "Alphabetical"
+        case .date: "Date"
+        }
+    }
+
+    func icon(isSelected: Bool, isAscending: Bool) -> String? {
+        guard isSelected else { return nil }
+        return isAscending ? "arrow.up" : "arrow.down"
+    }
 }
